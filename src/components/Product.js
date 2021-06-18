f95806cad10f07604b4516366a07b4ca2d62dcd4
mohsin <mohsin21_95@live.com>
2021-06-18 21:08:47 +0500

    --- Auto-Git Commit ---

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,27 +1,33 @@
-import React,{useEffect} from "react";
+import React,{useEffect,useState} from "react";
 
 import {Link,useParams} from "react-router-dom";
 import ImageGallery from 'react-image-gallery';
 import { Card, CardImg, CardBody,
   CardTitle, Container,Row,Button,Col,Label, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
+  import ReactPlayer from 'react-player'
   import SecureLS from 'secure-ls';
   var ls = new SecureLS({encodingType: 'aes'});
   var data = require('../assets/data.json');
   var link = 'http://10.0.1.105/cowmandipk/';
-  var temp=[]
+  var temp =[]
   export default function Child() {
     // We can use the `useParams` hook here to access
     // the dynamic pieces of the URL.
     let { id } = useParams();
+    var [count, setCount] =useState(0);
     useEffect(() => {    
       data.map((e)=>{
       if(e.id==id){
+        if(temp.length==0){
+        setCount(count + 1)  
             for(var a=1;a<=e.pic_count;a++){
-                temp.push({"original": link+"1/1.jpeg",
-                          "thumbnail": link+id1/1.jpeg"})
+                temp.push({"original": link+id+'/'+a+'.jpeg',
+                          "thumbnail": link+id+'/'+a+'.jpeg'})
             }
-      }
+          }
+      } 
     }
+
       )
 
       });
@@ -30,7 +36,8 @@ import { Card, CardImg, CardBody,
       <div>
         <Container className="themed-container"  >
           <Row>
-        <ImageGallery items={data} />
+              <ImageGallery items={temp} />
+              <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
         </Row></Container>
       </div>
     );
